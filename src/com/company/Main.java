7c7662ee73c6a7161f7f7760cbbb48523216fa6a
YuPengLu <ylu1100@bths.edu>
2018-10-11 08:25:05 -0400

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Choose a number between 1-100 and I'll try to guess it.")
+    Scanner input = new Scanner(System.in);
+	int min = 1;
+	int max = 100;
+	while (!input.nextInt() == "correct") {
+	    int mid = (min + max)/2;
+	    System.
+	    // write your code here
+    }
     }
 }
