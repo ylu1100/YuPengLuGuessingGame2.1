ec258d2c686580d57b3d05c14db23dabd2abbb6d
yupeng <ylu1100@bths.edu>
2018-10-11 21:11:00 -0400

    what.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Choose a number between 1-100 and I'll try to guess it.");
-    Scanner input = new Scanner(System.in);
 	int min = 1;
 	int max = 100;
-	int mid = 0;
-	while (!(input.nextLine() == "correct")) {
-	    mid = (min + max)/2;
-	    System.out.println("Is your number " + mid + "?");
-	    if (input.nextLine() == "too low"){
-	        max = max/2;
-        }
-        if (input.nextLine()=="too high"){
-            min = max/2;
-        }// write your code here
-    }
-    System.out.println("Your number was "+ mid);
+	int x = 50;
+	String str = "";
+		Scanner input = new Scanner(System.in);
+		while (!(str == "correct")){
+			x = (min + max)/2;			System.out.println("Is your number " + x);
+			str = input.nextLine();
+		if (str == ">") {
+			min = min+x;
+		}
+		if (str == "<") {
+			max = max - x;
+		}
+
+	}
+    System.out.println(" i knew it " + x);
     }
 }
