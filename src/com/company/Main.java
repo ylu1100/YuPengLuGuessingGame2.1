d6049b951e0bee556798c8acb56041e1af1ade28
yupeng <ylu1100@bths.edu>
2018-10-14 15:54:40 -0400

    IT FINALLY WORKS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,23 +4,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	int min = 1;
-	int max = 100;
-	int x = 50;
-	String str = "";
+	public static void main(String[] args) {
+		int min = 0;
+		int max = 100;
+		int x = 50;
+		System.out.println("Is your number " + x);
 		Scanner input = new Scanner(System.in);
-		while (!(str == "correct")){
-			x = (min + max)/2;			System.out.println("Is your number " + x);
-			str = input.nextLine();
-		if (str == ">") {
-			min = min+x;
-		}
-		if (str == "<") {
-			max = max - x;
-		}
+		String ans = input.nextLine();
+		while (!(ans =="correct")) {
+			if (ans.equals("lower")) {
+				max = x;
+				x = (min + max) / 2;
+				System.out.println("Is it " + x);
+				ans = input.nextLine();
+			}
+			if (ans.equals("higher")) {
+				min = x;
+				x = (min + max) / 2;
+				System.out.println("Is it " + x);
+				ans = input.nextLine();
 
+			}
+		}
+		System.out.println("Your number was " + x);
 	}
-    System.out.println(" i knew it " + x);
-    }
 }
