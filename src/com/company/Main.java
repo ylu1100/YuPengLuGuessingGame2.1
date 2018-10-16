2fede9e8e305131d8d4cbe363a0ccb3c9234a49e
yupeng <ylu1100@bths.edu>
2018-10-15 20:43:59 -0400

    guessing 2 with name and number of guesses

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,24 +8,32 @@ public class Main {
 		int min = 0;
 		int max = 100;
 		int x = 50;
-		System.out.println("Is your number " + x);
+		int guesses = 1;
 		Scanner input = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = input.nextLine();
+		System.out.println("I will try to guess " + name + "'s number and " + name + " will type higher if I should go higher and lower if I should go lower. Once I guess your number, "+name+" will type correct" );
+		System.out.println("Is " + name + "'s number "+ x);
 		String ans = input.nextLine();
-		while (!(ans =="correct")) {
+		while (!(ans.equals("correct"))) {
 			if (ans.equals("lower")) {
 				max = x;
 				x = (min + max) / 2;
-				System.out.println("Is it " + x);
+				System.out.println("Is " + name + "'s number "+ x);
 				ans = input.nextLine();
+				guesses++;
 			}
 			if (ans.equals("higher")) {
 				min = x;
 				x = (min + max) / 2;
-				System.out.println("Is it " + x);
+				System.out.println("Is " + name + "'s number "+ x);
 				ans = input.nextLine();
+				guesses++;
 
 			}
+
 		}
-		System.out.println("Your number was " + x);
+		System.out.println(name + "'s number was "+ x);
+		System.out.println("I guessed your number in " +guesses+ " tries");
 	}
 }
